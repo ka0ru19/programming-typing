c82b692b773885789d75459ffea20e6402cc7edb
井上 航 <winoue4506@office.baycurrent.co.jp>
2020-07-17 17:43:26 +0900

    単語の説明が表示されるようにした

--- sketch.js
+++ sketch.js
@@ -1,74 +1,99 @@
 const subject = document.getElementById('subject');
-const timer =
-      document.getElementById('timer');
+const term = document.getElementById('term');
+const timer = document.getElementById('timer');
 const form = document.forms.typing;
 const textList = [
-  'Hello World',
-  'swift',
-  'kotlin',
-  'unity',
-  'c#',
-  'html',
-  'css',
-  'JavaScript',
-  'ruby',
-  'let',
-  'var',
-  'int',
-  'string',
-  'for',
-  'if',
-  'if else',
-  'array',
-  'list',
-  'print',
-  'log',
-  'public',
-  'private',
-  'true',
-  'false',
-  'class',
-  'struct',
-  'return'
+  ['swift', 'iPhone開発言語'],
+  ['kotlin', 'Android開発言語'],
+  ['unity', 'game開発ソフト'],
+  ['c#', 'game開発言語'],
+  ['html', 'web-pageマークアップ言語'],
+  ['css', 'web-pageスタイルシート'],
+  ['JavaScript', 'webpage操作言語'],
+  ['ruby', 'web-service開発言語'],
+  ['let', '定数の宣言'],
+  ['var', '変数の宣言'],
+  ['int', '整数'],
+  ['string', '文字列'],
+  ['for', '繰り返し処理'],
+  ['if else', '条件分岐'],
+  ['array', '配列'],
+  ['print', '出力'],
+  ['log', 'ログ'],
+  ['true', '真'],
+  ['false', '偽'],
+  ['class', 'クラス'],
+  ['struct', '構造体'],
+  ['return', '戻り']
 ];
- 
-let TIME = 5;
-let state = true;
+let TIME_LIMIT = 8;
 let correctList = [];
- 
-const countdown = setInterval(function() {
-  timer.textContent = '制限時間：' + --TIME + '秒';
-  if(TIME <= 0) finish();
-}, 1000);
- 
+let countdown;
+let gameStatus;
+const WAITING = 'waiting'
+const TYPING = 'typing'
+const FINISHED = 'finished'
+
+
 form.btn.addEventListener('click', function(e) {
-  if(!state) return;
+  if(gameStatus!==TYPING) return;
  
-  if(form.input.value === subject.textContent) {
-    correctList.push(subject.textContent);
-    init();
+  if(form.input.value === term.textContent) {
+    correctList.push(term.textContent);
+    setGame();
   } else {
     subject.textContent = '間違いです！';
-    setTimeout(function(){ init() },1000)
+    setTimeout(function(){ setGame() }, 1000)
   }
 });
  
+document.body.onkeyup = function(e){
+  if(gameStatus===WAITING && e.keyCode == 32){
+    // ゲーム開始待機中にスペースキー押下
+    setupGame();
+    setGame();
+  }
+  
+  if(gameStatus===FINISHED && e.keyCode == 27){
+    // ゲーム終了後にESCキー押下
+    init();
+  }
+}
+
 init();
  
 function init() {
+  gameStatus = WAITING;
+  correctList = [];
+  subject.textContent = 'space押下でgame start!';
+}
+
+function setGame() {
   const randomIndex = Math.floor(Math.random() * textList.length);
- 
-  subject.textContent = textList[randomIndex];
+
+  subject.textContent = textList[randomIndex][1];
+  term.textContent = textList[randomIndex][0];
   form.input.value = '';
   form.input.focus();
 }
+
+function setupGame() {
+  gameStatus = TYPING;
+  time = TIME_LIMIT;
+  countdown = setInterval(function() {
+    timer.textContent = '制限時間：' + --time + '秒';
+    if(time <= 0) finish();
+  }, 1000);
+  timer.textContent = '制限時間：' + TIME_LIMIT + '秒';
+}
  
 function finish() {
+  gameStatus = FINISHED;
   clearInterval(countdown);
   var sum = 0;
   for (const collectTerm of correctList) {
     sum += collectTerm.length;
   }
-  subject.textContent = '正解数：' + sum + '文字、' + correctList.length + '語！';
-  state = false;
+  subject.textContent = '正解数：' + sum + '文字、' + correctList.length + '用語！';
+  term.textContent = '';
 }
